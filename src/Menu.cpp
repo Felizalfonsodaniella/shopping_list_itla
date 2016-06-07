fd63e9399e0782b89f185a75067138ee8294f1e0
Daniella Feliz <Daniella Feliz>
2016-06-06 21:02:22 -0700

    updating solution

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -67,27 +67,66 @@ void Menu::routeAction(int option)
 
 void Menu::listItems()
 {
-	//TODO: Implement this
+	
 	cout << "Implement  Menu::listItems" << endl;
+	 system("cls");
+    Element* i = list -> getFirstElement();
+
+    while (i != NULL){
+        cout << i -> getCode() << ".- " << i -> getName() << " " << i -> getQuantity() <<endl;
+        i = i -> getNext();
+    }
+    Element *lista2 = list -> getFirstElement();
+    while (i != NULL){
+        cout << list2 -> getCode() << ".- " << list2 -> getName() << " " << list2 -> getQuantity() <<endl;
+        list2 = list2 -> getNext();
+    }
+    system("pause");
 }
 
 void Menu::addItems()
 {
-	//TODO: Implement this
+	
 	cout << "Implement  Menu::addItems" << endl;
+	system("cls");
+    string name;
+    int quantity;
+    cout<<"introduce articles" <<endl;
+    cout<<"\n enter the name of the article: ";
+    cin >> name;
+    cout<<"enter the amount of the new contact: ";
+    cin >> quantity;
+    Element* article = new Element(code, name, quantity);
+    list -> add(article);
+    code++;
+    system("pause");
 }
 
 void Menu::removeItems()
 {
-	//TODO: Implement this
+	
 	cout << "Implement  Menu::removeItems" << endl;
+	  system("cls");
+    int _code, Code;
+    cout<<"remove article\n ";
+    cout<<"Enter the code the article: ";
+    cin >> _code;
+    Element* i = list -> getFirstElement();
+    while (i != NULL){
+        if (_code == i -> getCode()) {
+            Element* article = new Element(i -> getCode(), i -> getName(), i -> getQuantity());
+            list -> remover(article);
+        }
+        i = i -> getNext();
+    }
+    system("pause");
 }
 
 void Menu::show()
 {
 	int option;
 	do
-	{
+	{	
 		clearScreen();
 		cout << "ITLA Shopping List" << endl;
 		cout << "1- List items" << endl;
